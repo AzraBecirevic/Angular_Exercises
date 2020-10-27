205a37338b3a40978899face82e6058e6fd31463
Azra <azra.becirevic@edu.fit.ba>
2020-10-27 15:12:49 +0100

    First commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-server',  // html tag koji dozvoljava da koristimo ovu komponentu u drugim komponentama
+    templateUrl:'./server.component.html'
+})
+export class ServerComponent{
+
+}
