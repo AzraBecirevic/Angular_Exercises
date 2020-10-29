769385184929eab36d6c29d38525bd028624aa7f
Azra <azra.becirevic@edu.fit.ba>
2020-10-29 14:41:05 +0100

    *ngIf used

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'TestServer';
+  serverCreated = false;
 
   constructor() { 
     setTimeout(()=>{this.allowNewServer=true},2000);
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
   
   onCreateServer(){
     this.serverCreationStatus = 'Server was created. Name is '+ this.serverName;
+    this.serverCreated = true;
   }
                              //any
   onUpdateServerName(event: Event){
