2b334d58c079709b7611762eb1b239a62fc30134
Azra <azra.becirevic@edu.fit.ba>
2020-10-29 14:00:37 +0100

    Databinding exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,18 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+For Two-Way-Binding to work, we need to enable the ngModel  directive. This is done by adding the FormsModule
+to the imports[]  array in the AppModule.
+
+We then also need to add the import from @angular/forms  in the app.module.ts file:
+import { FormsModule } from '@angular/forms'; 
+*/
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,30 @@ import { Component, OnInit } from '@angular/core';
   // selector: 'app-servers'
   //selector: '[app-servers]',
   selector: '.app-servers',
-  template:'<app-server></app-server><app-server></app-server>',       // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
+  //template:'<app-server></app-server><app-server></app-server>',      
+                                                           // template: ``  moze se preci u red ispod
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+   
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
 
-  constructor() { }
+  constructor() { 
+    setTimeout(()=>{this.allowNewServer=true},2000);
+  }
 
   ngOnInit(): void {
   }
-
+  
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created. Name is '+ this.serverName;
+  }
+                             //any
+  onUpdateServerName(event: Event){
+     //console.log(event);
+     this.serverName=(<HTMLInputElement>event.target).value;
+  }
 }
